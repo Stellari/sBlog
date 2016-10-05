d249a9a11f3a9aaf349103cd381a3132703aaa44
Stellari Lee <hacklee@126.com>
2016-10-05 13:36:25 +0800

    设置文章只显示标题，描述，内容

--- Blog/admin.py
+++ Blog/admin.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    fields = ('title','desc','content',)
+
 admin.site.register(User)
 admin.site.register(Tag)
 admin.site.register(Category)
-admin.site.register(Article)
+admin.site.register(Article,ArticleAdmin)
 admin.site.register(Comment)
 admin.site.register(Links)
 admin.site.register(Ad)
