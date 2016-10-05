e0abc30559a9c98d7e8a225a91bc252725f4fd95
Stellari Lee <hacklee@126.com>
2016-10-05 13:03:14 +0800

    增加二级路由
    导入include，为sBlog设置二级路由

--- sBlog/settings.py
+++ sBlog/settings.py
@@ -84,6 +84,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME':'sblog',
+        # 'USER':'root',
+        # 'PASSWORD':'525458',
+        # 'HOST':'127.0.0.1',
+        # 'PORT':'3306',
+
     }
 }
 
